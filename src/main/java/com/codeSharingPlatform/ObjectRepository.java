ed856a771d6900bc310c480a52ecaccbe42572ad
Mika <mikari00@gamil.com>
2022-08-09 11:59:14 +0200

    Changed ObjectRepository

--- src/main/java/com/codeSharingPlatform/ObjectRepository.java
+++ src/main/java/com/codeSharingPlatform/ObjectRepository.java
@@ -3,6 +3,6 @@ package com.codeSharingPlatform;
 import java.util.LinkedHashMap;
 
 public interface ObjectRepository<T> {
-    public Integer storeCode(T t);
-    public T getCodeByID(Integer id);
+    Integer storeCode(T t);
+    T getCodeByID(Integer id);
 }
